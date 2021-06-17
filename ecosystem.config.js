ffda4f8d0a962a2ec233b609bfcc08e8aa5fac24
elinberg <elinberg@gmail.com>
2021-06-17 09:05:15 -0500

    eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       key: '~/.ssh/node.pem',
       ref  : 'origin/main',
       repo : 'git@github.com:elinberg/signal.git',
-      path : '/home/node/signal/source',
+      path : '/home/node/signal',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ~/signal/ecosystem.config.js --env production',
       'pre-setup': ''
